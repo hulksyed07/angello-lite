4d064b57ccb8edd9b0a645077ee6e5ac7ef23c6a
hulksyed07 <hulk.syed07@gmail.com>
2017-04-24 15:46:18 +0530

    Testing Dragstart event

--- public/storyboard/directives/StoryboardDirective.js
+++ public/storyboard/directives/StoryboardDirective.js
@@ -1,7 +1,14 @@
-angular.module('Angello.Storyboard').directive('story', function() {
+angular.module('Angello.Storyboard').directive('story', function(StoryModel) {
+    var linker = function(scope, elem, attrs) {
+    	elem.on('dragstart', function(event){
+    		console.log(event);
+    	})
+    }
+
     return {
         scope: true,
         replace: true,
-        template: '<div><h4>{{story.title}}</h4><p>{{story.description}}</p></div>'
+        link: linker,
+        template: '<div draggable="true"><h4>{{story.title}}</h4><p>{{story.description}}</p></div>'
     };
 });
